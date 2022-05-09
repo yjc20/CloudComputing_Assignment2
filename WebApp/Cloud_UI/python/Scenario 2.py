e09a345334c5c5b23414601fb6b339f1446954d1
rsangwa <103744464+rsangwa@users.noreply.github.com>
2022-05-09 18:46:37 +1000

    Update Scenario 2.py
    Added Code

--- WebApp/Cloud_UI/python/Scenario 2.py
+++ WebApp/Cloud_UI/python/Scenario 2.py
@@ -1,2 +1,31 @@
 import numpy as py;
 import plotly as plot;
+import matplotlib.pyplot as plt;
+import pandas as pd;
+
+# Read Data
+ crime_df = pd.read_json("/results_data/tweets_crime")
+  
+# Generate bar plots
+def generate_bar_plots():
+ 
+  barWidth = 0.4
+
+  #Need to initialize
+  x_value1 = 
+  x_value2 = 
+  
+  #Need to check this
+  y_value1 = crime_df['results']
+  y_value2 = crime_df['results']
+
+  plt.bar(x_value1, y_value1, width = barWidth, color = 'blue', edgecolor = 'black', capsize=7, label='Subjectivity Score')
+  
+  plt.bar(x_value2, y_value2, width = barWidth, color = 'red', edgecolor = 'black', capsize=7, label='Crime rate')
+  
+  #Initialize the values
+  plt.xticks(, ['', '', '', '', ''])
+  plt.ylabel()
+  plt.legend()
+  
+  plt.show()
