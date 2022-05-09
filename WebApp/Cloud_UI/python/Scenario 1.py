5acd57d8a93fe7bbee4589515f6b08bb0880a518
rsangwa <103744464+rsangwa@users.noreply.github.com>
2022-05-09 18:24:00 +1000

    Update Scenario 1.py
    Added code

--- WebApp/Cloud_UI/python/Scenario 1.py
+++ WebApp/Cloud_UI/python/Scenario 1.py
@@ -1,2 +1,18 @@
 import numpy as py;
 import plotly as plot;
+
+import seaborn as sbn
+sbn.set_theme
+#Covid HeatMap
+
+#Read COVID related data
+
+df_covid = pd.read_csv("./result_data/tweets_covid")
+
+#Generate heatmap for Covid related data
+def generate_heatmap():
+  ax = sbn.heatmap(df_covid)
+  return ax
+  
+
+
