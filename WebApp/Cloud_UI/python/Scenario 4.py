89049533ad32f30de7f13767a68fe08d565e8f0c
rsangwa <103744464+rsangwa@users.noreply.github.com>
2022-05-11 16:08:09 +1000

    Update Scenario 4.py

--- WebApp/Cloud_UI/python/Scenario 4.py
+++ WebApp/Cloud_UI/python/Scenario 4.py
@@ -1,2 +1,11 @@
-import numpy as py;
-import plotly as plot;
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_pie_chart():
+  #Read the data
+  df_election = pd.read_csv("./result_data/election.csv")
+  result = df_election['tpp_australian_labor_party_percentage', ' tpp_liberal_national_coalition_percentage']].loc[0]
+  labels = ['tpp_australian_labor_party_percentage', ' tpp_liberal_national_coalition_percentage']
+  plt.pie(result, labels = labels, shadow = True, autopct='%1.1f%%')
+  plt.legend()
+  plt.show()
