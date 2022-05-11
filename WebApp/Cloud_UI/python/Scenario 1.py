793eeffa171bc1b1506ea384dcfaa5636e85e2fb
rsangwa <103744464+rsangwa@users.noreply.github.com>
2022-05-11 14:56:42 +1000

    Update Scenario 1.py

--- WebApp/Cloud_UI/python/Scenario 1.py
+++ WebApp/Cloud_UI/python/Scenario 1.py
@@ -2,13 +2,12 @@ import numpy as py;
 import pandas as pd
 import seaborn as sbn
 sbn.set_theme()
-#Covid HeatMap
 
-#Read COVID related data
-df_covid = pd.read_csv("./result_data/tweets_covid.csv")
-
-#Generate heatmap for Covid related data
+#Generate heatmap
 def generate_heatmap():
+  
+  #Read COVID related data
+  df_covid = pd.read_csv("./result_data/filename.csv")
   ax = sbn.heatmap(df_covid[['subjectivity', 'polarity']])
   return ax
 
