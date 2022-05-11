d20d7e1080985fdeda06881dab8cc3950c81c465
rsangwa <103744464+rsangwa@users.noreply.github.com>
2022-05-11 16:18:11 +1000

    Update Scenario 4.py

--- WebApp/Cloud_UI/python/Scenario 4.py
+++ WebApp/Cloud_UI/python/Scenario 4.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def generate_pie_chart():
   #Read the data
   df_election = pd.read_csv("./result_data/election.csv")
-  result = df_election['tpp_australian_labor_party_percentage', ' tpp_liberal_national_coalition_percentage']].loc[0]
+  result = df_election[['tpp_australian_labor_party_percentage', ' tpp_liberal_national_coalition_percentage']].loc[0]
   labels = ['tpp_australian_labor_party_percentage', ' tpp_liberal_national_coalition_percentage']
   plt.pie(result, labels = labels, shadow = True, autopct='%1.1f%%')
   plt.legend()
