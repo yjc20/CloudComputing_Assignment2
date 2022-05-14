84c4b8da577a7a97ed4cd783ba35932c68f9d033
rsangwa <103744464+rsangwa@users.noreply.github.com>
2022-05-15 00:46:42 +1000

    Update Scenario 5.py

--- WebApp/Cloud_UI/python/Scenario 5.py
+++ WebApp/Cloud_UI/python/Scenario 5.py
@@ -1,16 +1,2 @@
-import pandas as pd;
-import matplotlib.pyplot as plt;
-from wordcloud import WordCloud
 
-def generate_word_cloud():
-  #Read the data
-  df_hashtags = pd.read_csv("/result_data/hashtags.csv")
-  text = df_hashtags['hashtags'].to_string()
-  
-  # Create and generate a word cloud image:
-  wordcloud = WordCloud().generate(text)
 
-  # Display the generated image:
-  plt.imshow(wordcloud, interpolation='bilinear')
-  plt.axis("off")
-  plt.show()
