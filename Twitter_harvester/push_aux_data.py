d7a283c1f2f4e8774183e09bcbf3112f9c2f72a3
archit-verma <46745100+archit-verma@users.noreply.github.com>
2022-05-15 00:09:25 +1000

    Update push_aux_data.py
    add counts and sentiment analysis

--- Twitter_harvester/push_aux_data.py
+++ Twitter_harvester/push_aux_data.py
@@ -8,7 +8,10 @@ Created on Sat May 14 17:12:45 2022
 
 import json
 from dbconnector import Couch
-
+from textblob import TextBlob
+from nltk.sentiment import SentimentIntensityAnalyzer
+import nltk
+nltk.download("vader_lexicon")
 
 
 # ip address for the master node from ip.txt file
@@ -20,6 +23,22 @@ ip_file.close()
 # couchdb connection class instance created
 couch=Couch('http://'+couchdb_master_ip+':5984/',['tweet'])
 
+def preprocess(text):
+    text = re.sub("@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+", ' ', str(text).lower()).strip()
+    return text
+
+def subjectivity(text):
+    text = preprocess(text)
+    subjectivity = TextBlob(text).sentiment.subjectivity
+    return subjectivity
+    
+    
+def polarity(text):
+    sia = SentimentIntensityAnalyzer()
+    text = preprocess(text)
+    polarity = sia.polarity_scores(text)['compound']
+    return polarity
+
 
 try :
     f = open('tweets.json')
@@ -27,9 +46,10 @@ try :
     tweets_collector = []
     
     for i in file['rows']:
-        tweet = {"tweet_id":i['tweet_id'], "text":i["text"],"city":i["city"]}
+        tweet = {"tweet_id":i['tweet_id'], "text":i["text"],"city":i["city"],"subjectivity":subjectivity(i["text"]),
+                 "polarity":polarity(i["text"]), "retweet_count":i["retweet_count"], "favorite_count":i["favorite_count"]}
         tweets_collector.append(tweet)
-    
+
     counter = 0
     for i in tweets_collector:
         couch.pushdata(i,'tweet')
