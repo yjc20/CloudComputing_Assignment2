4f8e474d9a3ea9ab209bef69b4ec772c143a9c6a
YJC\HP <yateenchiplunkar2010@gmail.com>
2022-05-15 15:04:05 +1000

    Updates

--- WebApp/Cloud_UI/python/webapp.py
+++ WebApp/Cloud_UI/python/webapp.py
@@ -1,40 +1,49 @@
-
 import matplotlib.pyplot as plt
+import render
 from flask import Flask, render_template, send_file, request, url_for, redirect
 import json
 
-from Scenario import twin_barplots, generate_heatmap, generate_bar_plots, generate_line_graphs, generate_pie_chart, generate_word_cloud
+from Scenario import twin_barplots, generate_heatmap, generate_bar_plots, generate_line_graphs, \
+    generate_pie_chart, generate_word_cloud
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def display_index():
-  return render.template("Home.html")
+    return render.template("Home.html")
+
 
 @app.route('/scenario1')
 def display_heatmap():
-  ax = generate_heatmap()
-  return ax
+    ax = generate_heatmap()
+    return ax
+
 
 @app.route('/scenario2')
 def display_barplot():
-  return generate_bar_plots()
+    return generate_bar_plots()
+
 
 @app.route('/scenario3')
 def display_line_graphs():
-  return generate_line_graphs()
+    return generate_line_graphs()
+
 
 @app.route('/scenario4')
 def display_piechart():
-  return generate_pie_chart()
+    return generate_pie_chart()
+
 
 @app.route('/wordcloud')
 def wordcloud():
-  return generate_word_cloud()
+    return generate_word_cloud()
+
 
 @app.route('/scenario5')
 def display_twin_barplots():
-  return twin_barplots()
-if __name__ == '__main__':
-    app.run(debug=True,use_reloader=True)
+    return twin_barplots()
 
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
